68942f9a470649c419a75ea887f30c0237a56785
giraffon93 <monjet.jeremy@hotmail.com>
2026-06-04 21:24:23 +0200

    Create app.js

---
+++ app.js
@@ -0,0 +1,27 @@
+// Version globale
+const APP_VERSION = "1.1.2";
+
+// Affichage version dans toutes les pages
+window.addEventListener("DOMContentLoaded", () => {
+    const v = document.getElementById("app-version");
+    if (v) v.textContent = APP_VERSION;
+});
+
+// Fonction mise à jour PWA
+function updateApp(){
+    if ('serviceWorker' in navigator) {
+        navigator.serviceWorker.getRegistration().then(reg => {
+            if (reg) {
+                reg.update();
+                alert("Mise à jour demandée.\nRouvrez l’app dans 2 secondes.");
+            }
+        });
+    }
+}
+
+// Auto‑reload après mise à jour
+if ("serviceWorker" in navigator) {
+    navigator.serviceWorker.addEventListener("controllerchange", () => {
+        location.reload();
+    });
+}
