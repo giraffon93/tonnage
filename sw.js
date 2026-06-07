e87c4ba0262f4cbb642add5f8816c8a892328fff
giraffon93 <monjet.jeremy@hotmail.com>
2026-06-07 13:07:09 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// build 4
-const CACHE_NAME = "tonnage-m3-v4";
+// build 5
+const CACHE_NAME = "tonnage-m3-v5";
 
 self.addEventListener("install", event => {
   self.skipWaiting(); // active immédiatement la nouvelle version
